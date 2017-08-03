955d54a857228545d06460eb38dd8130206fabdd
jbtule <jay+code@tuley.name>
2017-08-03 14:01:43 -0500

    Basic Official Keyzar Schema

---
+++ Keyczar/Keyczar/KeySet/Metadata/OfficialKeyVersion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Keyczar
+{
+    internal class OfficialKeyVersion
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KeyVersion"/> class.
+        /// </summary>
+        public OfficialKeyVersion()
+        {
+            Status = KeyStatus.Active;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KeyVersion"/> class.
+        /// </summary>
+        /// <param name="keyVersion">The key version.</param>
+        public OfficialKeyVersion(KeyVersion keyVersion)
+        {
+            VersionNumber = keyVersion.VersionNumber;
+            Exportable = keyVersion.Exportable;
+            Status = keyVersion.Status;
+        }
+
+        /// <summary>
+        /// Gets or sets the version number.
+        /// </summary>
+        /// <value>The version number.</value>
+        public int VersionNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="KeyVersion"/> is exportable.
+        /// </summary>
+        /// <value><c>true</c> if exportable; otherwise, <c>false</c>.</value>
+        public bool Exportable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status.
+        /// </summary>
+        /// <value>The status.</value>
+        public KeyStatus Status { get; set; }
+
+    }
+
+}
