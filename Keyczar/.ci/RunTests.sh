d468df4b938363121e80f7cdde25aeb898f51d01
jbtule <jay.tuley@ekonbenefits.com>
2019-07-26 17:49:23 -0500

    more test updates for CI

--- Keyczar/.ci/RunTests.sh
+++ Keyczar/.ci/RunTests.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -x
 
 runTest(){
-    mono --runtime=v4.0 packages/nunit.runners/2.6.1/tools/nunit-console.exe -noxml -nodots -labels $@
+    mono --runtime=v4.0 packages/nunit.runners/3.10.1/tools/nunit3-console.exe -noxml -nodots -labels $@
    if [ $? -ne 0 ]
    then   
      exit 1
    fi
 }
 
-runTest KeyczarTest/bin/Debug/net40/KeyczarTest.dll -exclude=Performance
+runTest KeyczarTest/bin/Debug/net451/KeyczarTest.dll -exclude=Performance
 
 exit $?
