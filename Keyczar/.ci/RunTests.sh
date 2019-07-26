ff3d0494a4cd4d13e59c9275ecae0c6438e20c0a
jbtule <jay.tuley@ekonbenefits.com>
2019-07-26 17:56:46 -0500

    update flags

--- Keyczar/.ci/RunTests.sh
+++ Keyczar/.ci/RunTests.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -x
 
 runTest(){
-    mono --runtime=v4.0 packages/nunit.runners/3.10.1/tools/nunit3-console.exe -noxml -nodots -labels $@
+    mono --runtime=v4.0 packages/nunit.runners/3.10.1/tools/nunit3-console.exe --noresult --labels=On --workers=1 $@
    if [ $? -ne 0 ]
    then   
      exit 1
    fi
 }
 
-runTest KeyczarTest/bin/Debug/net451/KeyczarTest.dll -exclude=Performance
+runTest KeyczarTest/bin/Debug/net451/KeyczarTest.dll --where "cat != Performance" 
 
 exit $?
