9790b4f72e74e5ba053215c1019a1190e9ffb5af
jbtule <jay.tuley@ekonbenefits.com>
2019-07-31 09:37:04 -0500

    remove old publish scripts

--- Keyczar/publish/PublishNuget.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-IF %2.==. GOTO WrongArgs
-
-..\.nuget\nuget.exe push %1.nupkg %2
-..\.nuget\nuget.exe push %1.symbols.nupkg %2 -source http://nuget.gw.symbolsource.org/Public/NuGet
-
-:WrongArgs
-ECHO "PublishNuget <pkgname> <apikey>"
